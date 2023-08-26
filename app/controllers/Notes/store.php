350ceb7e6d1f7134986a26a86e6c7a6084f77184
Hart <hart.hpwebdev@gmail.com>
2023-08-26 21:25:11 +0800

    Fix: uid of note author

--- app/controllers/Notes/store.php
+++ app/controllers/Notes/store.php
@@ -6,6 +6,8 @@ use Core\Validator;
 
 $db = App::resolve(Database::class);
 
+$current_user_id = getCurrentUserID();
+
 $errors = [];
 
 if (!Validator::string($_POST['body'], 1, 1000)) $errors['body'] = 'A body of no more than 1000 characters is required!';
@@ -19,8 +21,8 @@ if (!empty($errors)) {
 
 $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
   'body' => $_POST['body'],
-  'user_id' => 4,
+  'user_id' => $current_user_id,
 ]);
 
-header("location: /");
+redirect('/');
 exit();
