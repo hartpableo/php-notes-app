71fa339bf7a22c8031583e48415d54702d9ae9a1
Hart <hart.hpwebdev@gmail.com>
2023-08-27 18:21:32 +0800

    Composer installed + Utilized composer's built-in autoloader

--- app/Core/init.php
+++ app/Core/init.php
@@ -1,16 +1,7 @@
 <?php
 
-// Class Autoloader
-spl_autoload_register(function ($className) {
-  $classPath = str_replace('\\', DIRECTORY_SEPARATOR, $className);
-  $filePath = base_path("app/{$classPath}.php");
-
-  if (file_exists($filePath)) {
-    require_once $filePath;
-  }
-});
-
 require_once 'config.php';
+require_once BASE_PATH . '/vendor/autoload.php';
 require_once 'Response.php';
 require_once 'Validator.php';
 require_once 'functions.php';
